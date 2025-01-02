47527551482f0dbdf58d4570b1cfee89045b61aa
Artur Castro <arturcastro010@gmail.com>
2025-01-02 09:51:40 -0300

    Base de criação de jogo feita + configuração básica do projeto + início da classe Sudoku

--- src/main/java/br/ufjf/dcc/dcc025/Main.java
+++ src/main/java/br/ufjf/dcc/dcc025/Main.java
@@ -1,21 +1,45 @@
 package br.ufjf.dcc.dcc025;
 import javax.swing.JOptionPane;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String nome = JOptionPane.showInputDialog("Digite seu nome: ");
-        JOptionPane.showMessageDialog(null,"Bem-vindo, " + nome + "!");
+        //Criação do tabuleiro
+        Sudoku table = new Sudoku();
+
+        /*String nome = JOptionPane.showInputDialog("Digite seu nome: ");
+        JOptionPane.showMessageDialog(null,"Bem-vindo, " + nome + "!");*/
 
         String option = JOptionPane.showInputDialog("Forma de começar o jogo: \n (1) Gerar um jogo aleatório \n (2) Definir o próprio jogo");
         switch (option) {
             case "1": String sortNumber = JOptionPane.showInputDialog("Quantos números você deseja sortear?");
             break;
-            case "2": JOptionPane.showMessageDialog(null,"Definindo seu próprio jogo! (digitar no console)");
-            break;
-            default: JOptionPane.showMessageDialog(null,"Opção inválida!");
+
+            case "2": {
+                Scanner scanner = new Scanner(System.in);
+                List<int[]> entries = new ArrayList<>();
+                JOptionPane.showMessageDialog(null, "Definindo seu próprio jogo! (digitar no console)");
+                System.out.println("Insira os valores no formato (linha,coluna,valor), sendo que (-1,-1,-1) encerra a inserção.");
+
+                while (true) {
+                    String input = scanner.nextLine();
+
+                    if (input.equals("(-1,-1,-1)")) {
+                        break;
+                    }
+
+                    Sudoku.gameMake(input, table);
+                    //Input em string é convertido para um array de arrays de informação e o jogo é criado com os valores em cada posição
+                }
+                break;
+            }
+
+            default: JOptionPane.showMessageDialog(null,"Opção inválida, escolha 1 ou 2!");
             break;
         }
+
 
 
 
--- src/main/java/br/ufjf/dcc/dcc025/Sudoku.java
+++ src/main/java/br/ufjf/dcc/dcc025/Sudoku.java
@@ -3,7 +3,31 @@ package br.ufjf.dcc.dcc025;
 public class Sudoku {
     private int[][] table;
 
-    public Sudoku(){
+    public Sudoku() {
         this.table = new int[9][9];
+    }
+
+    public static void gameMake(String input, Sudoku table) { //Formata as entradas para receber os valores corretamente
+        String[] tuplas = input.split("\\)\\("); //Dá split em cada )( pra separar os grupos;
+
+        tuplas[0] = tuplas[0].substring(1); //Remove parêntese inicial
+        tuplas[(tuplas.length) - 1] = tuplas[tuplas.length - 1].substring(0, tuplas[tuplas.length - 1].length() - 1); //Remove parêntese final
+
+        int[][] group = new int[tuplas.length][3]; //Array de arrays com espaço para o número de strings presentes em "tuplas" e informações que serão extraídas de cada uma
+        for (int i=0; i<tuplas.length; i++) {
+            String[] stringNumbers = tuplas[i].split(",");
+
+            for (int j=0; j<3; j++) {
+                group[i][j] = Integer.parseInt(stringNumbers[j]); // Converter cada parte em um inteiro
+            }
+        }
+
+        table.assignValues(group);
+    }
+
+    private void assignValues(int[][] group) {
+        for (int[] info : group){
+            this.table[info[0]][info[1]] = info[2];
+        }
     }
 }
